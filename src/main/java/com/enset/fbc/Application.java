ba48f3cbf13261ece433c70f6817b1b1c2063e77
yassine-mabrouk <mabroukyassine70@gmail.com>
2022-11-19 06:05:58 +0100

    crypter password

--- src/main/java/com/enset/fbc/Application.java
+++ src/main/java/com/enset/fbc/Application.java
@@ -3,9 +3,9 @@ package com.enset.fbc;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 @SpringBootApplication
@@ -14,7 +14,10 @@ public class Application implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
     @Override
     public void run(String... args) throws Exception {
 
--- src/main/java/com/enset/fbc/serviceImpli/UserServiceImpl.java
+++ src/main/java/com/enset/fbc/serviceImpli/UserServiceImpl.java
@@ -7,19 +7,22 @@ import com.enset.fbc.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
-
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public UserEntity createUser(UserEntity userDto) {
         UserEntity checkUser = userRepository.findByEmail(userDto.getEmail());
         // verifier existance of the user in BD
         if (checkUser!=null) throw  new RuntimeException("User existe !!!");
+        userDto.setPassword(bCryptPasswordEncoder.encode(userDto.getPassword()));
          UserEntity saved = userRepository.save(userDto);
           return saved;
     }
